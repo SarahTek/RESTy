61e28be2c9d590600d8fbbb3e4a314d5e54a57c9
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2022-10-03 00:59:37 -0400

    finished lab 26

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-
-class Footer extends React.Component {
-  render() {
-    return <footer>&copy; 2018</footer>;
-  }
+import './footer.scss'
+function Footer() {
+  return <footer>Sarah Teklemariam &copy; 2018</footer>;
 }
 
 export default Footer;
