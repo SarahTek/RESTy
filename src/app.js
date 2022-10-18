59e9bde6813f98cb88fb7eda4d2b280c791c5df3
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2022-10-18 00:24:37 -0400

    completed lab 28

--- src/app.js
+++ src/app.js
@@ -1,38 +1,64 @@
-import React, { useState } from 'react';
 
+
+import React, { useEffect, useState } from 'react';
 import './app.scss';
-
-// Let's talk about using index.js and some other name in the component folder
-// There's pros and cons for each way of doing this ...
 import Header from './components/header';
 import Footer from './components/footer';
 import Form from './components/form';
 import Results from './components/results';
+import axios from 'axios';
+
 
 function App() {
   const [data, setData] = useState(null);
-  const [requestParams, setRequestParams] = useState({})
+  const [requestParams, setRequestParams] = useState({});
 
 
-  const callApi = (requestParams) => {
-    // mock output
-    const payload = {
-      count: 2,
-      results: [
-        { name: 'fake thing 1', url: 'http://fakethings.com/1' },
-        { name: 'fake thing 2', url: 'http://fakethings.com/2' },
-      ],
-    };
-    setData(payload);
-    setRequestParams(requestParams);
+  const callApi = (receivingObjOnForm) => {
+    setRequestParams(receivingObjOnForm);
   }
+  useEffect(() => {
+    async function fetchData() {
+      if (requestParams.method === 'GET') {
+        await axios.get(requestParams.url).then(response => {
+          console.log(response);
+          setData(response);
+        });
+      }
+      if (requestParams.method === 'POST') {
+        await axios.post(requestParams.url, requestParams.body).then(response => {
+          console.log(response);
+          setData(response);
+        });
+      }
+      if (requestParams.method === 'PUT') {
+        await axios.put(requestParams.url).then(response => {
+          console.log(response);
+          setData(response);
+        });
+      }
+      if (requestParams.method === 'DELETE') {
+        await axios.delete(requestParams.url).then(response => {
+          console.log(response);
+          setData(response);
+        });
+      }
+
+    }
+    fetchData();
+
+
+  }, [requestParams]);
+
+
+
   return (
     <React.Fragment>
       <Header />
       <div>Request Method: {requestParams.method}</div>
       <div>URL: {requestParams.url}</div>
       <Form handleApiCall={callApi} />
-      <Results data={data} />
+      <Results dataFromPayload={data} />
       <Footer />
     </React.Fragment>
   );
